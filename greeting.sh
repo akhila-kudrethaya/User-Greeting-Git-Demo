2b90f63c40ee1220651d4109b4bfb36e8fe89419
Akhila Kudrethaya <118893197+akhila-kudrethaya@users.noreply.github.com>
2025-10-26 12:32:04 +0530

    First Commit to Sample greeting Program

---
+++ greeting.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+
+echo "Hello World!" 
+
+
+#Request user for name: 
+echo "What is your name? " 
+
+#Takes name as user input read name 
+read name
+
+#Greets the user with name that has been read 
+echo "Hello! Nice to meet you, $name!"
+
